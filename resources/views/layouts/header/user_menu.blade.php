062113873d6262659049fabe0296e1f1a452e074
ernie <dy99520@gmail.com>
2020-07-31 12:49:54 +0800

    App header layout

---
+++ resources/views/layouts/header/user_menu.blade.php
@@ -0,0 +1,11 @@
+<div class="menu">
+    <a class="item">
+        <i class="large child icon"></i>
+        個人檔案</a>
+    <a class="item">
+        <i class="large cog icon"></i>
+        設定</a>
+    <a class="item">
+        <i class="large sign out alternate icon"></i>
+        登出</a>
+</div>
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+        <title>@yield('title') | {{ config('app.name') }}</title>
+    </head>
+    <body>
+    @include('layouts.header.header')
+    <div class="ui container">
+        @yield('layout_content')
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
