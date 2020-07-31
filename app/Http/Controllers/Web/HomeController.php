4db1b845ed5117fd95e38a95be6f1a48a1258573
ernie <dy99520@gmail.com>
2020-07-31 12:17:09 +0800

    Home Controller and route

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    function index()
+    {
+        if (auth()->check()) {
+            return view('home'); //todo redirect to user home
+        } else {
+            return view('home');
+        }
+    }
 }
